b8224583cbeff9da79c4fe7af077bc788cb9dc91
frankfang <frankfang1990@gmail.com>
2017-04-08 16:16:06 +0800

    state

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,19 @@
 import React from 'react';
 class Welcome extends React.Component {
+  constructor(props){
+    super(props)
+    // 上面照抄，不懂的话看 MDN 
+    this.state = {
+      date: new Date()
+    }
+  }
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+        <h2>{this.state.date.toString()}</h2>
+      </div>
+    )
   }
 }
 
