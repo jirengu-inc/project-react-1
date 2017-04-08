1c979f4a64a586b73fa2cf48923f1b443de2d7ab
frankfang <frankfang1990@gmail.com>
2017-04-08 15:07:18 +0800

    add Welcome.js

---
+++ src/Welcome.js
@@ -0,0 +1,8 @@
+import React from 'react';
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, Component</h1>;
+  }
+}
+
+export default Welcome
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Welcome from './Welcome'
 
 ReactDOM.render(
-  <h1>Hi, World</h1>,
+  <Welcome/>,
   document.getElementById('root')
-);
+)
+
