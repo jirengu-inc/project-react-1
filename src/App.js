9f5d0d51155d6457d9814886aae2e0e403fdc3df
frankfang <frankfang1990@gmail.com>
2017-04-09 16:42:44 +0800

    fix value again

--- src/App.js
+++ src/App.js
@@ -23,19 +23,26 @@ class App extends Component {
         </li>
       )
     })
-    console.log(todos)
 
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
+          <TodoInput content={this.state.newTodo} 
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
         </ol>
       </div>
     )
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
   }
   addTodo(event){
     this.state.todoList.push({
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
   render(){
-    return <input type="text" defaultValue={this.props.content}
+    return <input type="text" value={this.props.content}
+      onChange={this.changeTitle.bind(this)}
       onKeyPress={this.submit.bind(this)}/>
   }
   submit(e){
     if (e.key === 'Enter') {
       this.props.onSubmit(e)
     }
+  }
+  changeTitle(e){
+    this.props.onChange(e)
   }
 }
 
