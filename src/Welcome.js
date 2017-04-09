b2c163d1753ff5997694fce89a80bc0ceba226e0
frankfang <frankfang1990@gmail.com>
2017-04-09 08:42:24 +0800

    展示钩子

--- src/Welcome.js
+++ src/Welcome.js
@@ -11,14 +11,23 @@ class Welcome extends React.Component {
         date: new Date() // 更新 date
       })
     })
+
+    console.log('我已经在 constructor 里将 props 和 state 初始化好了')
+  }
+  componentWillMount(){
+    console.log('运行到这里的话，说明马上就要运行 render 了')
   }
   render() {
+    console.log('嗯，这里是 render')
     return (
       <div>
         <h1>Hello, {this.props.name}</h1>
         <h2>{this.state.date.toString()}</h2>
       </div>
     )
+  }
+  componentDidMount(){
+    console.log('已经挂载到页面里了')
   }
 }
 
