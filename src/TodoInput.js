0f4f29af21d8db1fe5ff0cdef9af3226f4dfc944
frankfang <frankfang1990@gmail.com>
2017-04-09 16:04:18 +0800

    fix value

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
   render(){
-    return <input type="text" value={this.props.content}/>
+    return <input type="text" defaultValue={this.props.content}/>
   }
 }
 
