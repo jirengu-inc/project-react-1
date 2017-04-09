0f64c5e17162c7264fa7e69165c154b8fced82d1
frankfang <frankfang1990@gmail.com>
2017-04-09 17:10:09 +0800

    只展示没有被删除的todo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
   }
   render() {
 
-    let todos = this.state.todoList.map((item,index)=>{
+    let todos = this.state.todoList
+      .filter((item)=> !item.deleted)
+      .map((item,index)=>{
       return ( // 为什么这里要加个括号？这是动手题3 🐸
         <li key={index} >
           <TodoItem todo={item} onToggle={this.toggle.bind(this)} 
