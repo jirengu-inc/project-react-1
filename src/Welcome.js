aa7b9e3d1764a3cc62da259be547368de3cbd2f2
frankfang <frankfang1990@gmail.com>
2017-04-09 10:03:35 +0800

    确定哪些地方可以 setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,10 +7,12 @@ class Welcome extends React.Component {
       date: new Date(),
       test: '1'
     }
-    this.setState({
-      date: new Date(), // 更新 date
-      test: 'constructor'
-    })
+    setInterval(()=>{
+      this.setState({
+        date: new Date(), // 更新 date
+        test: 'setInterval'
+      })
+    },5000)
 
     console.log('我已经在 constructor 里将 props 和 state 初始化好了')
   }
@@ -53,18 +55,11 @@ class Welcome extends React.Component {
       date: new Date(), // 更新 date
       test: 'shouldComponentUpdate'
     })
+    return true
   }
   componentWillUpdate(){
-    this.setState({
-      date: new Date(), // 更新 date
-      test: 'componentWillUpdate'
-    })
   }
   componentDidUpdate(){
-    this.setState({
-      date: new Date(), // 更新 date
-      test: 'componentDidUpdate'
-    })
   }
   componentWillUnmount(){
     console.log('要死了')
