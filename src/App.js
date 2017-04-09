4418388d57cea78bb6b6be069d5f43c556acdccd
frankfang <frankfang1990@gmail.com>
2017-04-09 17:17:11 +0800

    更新 TodoItem 的样式

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
             onChange={this.changeTitle.bind(this)}
             onSubmit={this.addTodo.bind(this)} />
         </div>
-        <ol>
+        <ol className="todoList">
           {todos}
         </ol>
       </div>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import './TodoItem.css'
 
 export default class TodoItem extends Component {
   render(){
     return (
-      <div>
-      <input type="checkbox" checked={this.props.todo.status === 'completed'}
-        onChange={this.toggle.bind(this)}/> {this.props.todo.title}
-      <button onClick={this.delete.bind(this)}>删除</button>
+      <div className="TodoItem">
+        <input type="checkbox" checked={this.props.todo.status === 'completed'}
+          onChange={this.toggle.bind(this)}/> 
+        <span className="title">{this.props.todo.title}</span>
+        <button onClick={this.delete.bind(this)}>删除</button>
       </div>
     )
   }
