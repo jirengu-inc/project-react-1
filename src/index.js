f6d0c09e84922be2b700ded4ffdf8a48f850b422
frankfang <frankfang1990@gmail.com>
2017-04-09 14:52:47 +0800

    删除 Welcome

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Welcome from './Welcome'
+import App from './App'
 
 ReactDOM.render(
-  <Welcome name="frank"/>,
+  <App />,
   document.getElementById('root')
 )
 
