7ffd7f627071012ec7529da3909e6c75a6481cc6
frankfang <frankfang1990@gmail.com>
2017-04-17 10:55:08 +0800

    测试 leancloud 功能

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ AV.init({
   appKey: APP_KEY
 })
 
+var TestObject = AV.Object.extend('TestObject')
+var testObject = new TestObject()
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!')
+})
+
 class App extends Component {
   constructor(props){
     super(props)
