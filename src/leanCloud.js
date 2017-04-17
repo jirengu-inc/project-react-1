c44550a11ff2e80155714e6f85ccb3a8ed6effa8
frankfang <frankfang1990@gmail.com>
2017-04-17 11:00:34 +0800

    add ID and KEY

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -1,7 +1,7 @@
 import AV from 'leancloud-storage'
 
-var APP_ID = 你的ID
-var APP_KEY = 你的KEY
+var APP_ID = 'rUcmyykR2Tsi5VOPjfnkM7Yj-gzGzoHsz'
+var APP_KEY = 'Oy9HGah8eknBB6yTb9LOKjrt'
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
