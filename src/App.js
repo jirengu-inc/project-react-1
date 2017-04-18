cb086d4d6557a12c85876192fde3ea0f275b6048
frankfang <frankfang1990@gmail.com>
2017-04-18 16:00:28 +0800

    如果注册成功就关闭 UserDialog

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        <UserDialog onSignUp={this.onSignUp.bind(this)}/>
+        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
       </div>
     )
   }
