10988f7b9471da7879b0b064da327e4895c5cde9
frankfang <frankfang1990@gmail.com>
2017-04-18 16:21:51 +0800

    展示友好的错误信息

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -24,7 +24,14 @@ export default class UserDialog extends Component{
       this.props.onSignUp.call(null, user)
     }
     let error = (error)=>{
-      alert(error)
+      switch(error.code){
+        case 202:
+          alert('用户名已被占用')
+          break
+        default:
+          alert(error)
+          break
+      }
     }
     signUp(username, password, success, error)
   }
@@ -35,7 +42,14 @@ export default class UserDialog extends Component{
       this.props.onSignIn.call(null, user)
     }
     let error = (error)=>{
-      alert(error)
+      switch(error.code){
+        case 210:
+          alert('用户名与密码不匹配')
+          break
+        default:
+          alert(error)
+          break
+      }
     }
     signIn(username, password, success, error)
   }
