276ebc574eea9accf12152e5767fedf99ec763ef
frankfang <frankfang1990@gmail.com>
2017-04-18 16:09:50 +0800

    添加登出功能

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import UserDialog from './UserDialog'
-import {getCurrentUser} from './leanCloud'
+import {getCurrentUser, signOut} from './leanCloud'
 
 class App extends Component {
   constructor(props){
@@ -31,7 +31,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>{this.state.user.username||'我'}的待办</h1>
+        <h1>{this.state.user.username||'我'}的待办
+          {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
+        </h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo} 
             onChange={this.changeTitle.bind(this)}
@@ -43,6 +45,12 @@ class App extends Component {
         {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
       </div>
     )
+  }
+  signOut(){
+    signOut()
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = {}
+    this.setState(stateCopy)
   }
   onSignUp(user){
     let stateCopy = JSON.parse(JSON.stringify(this.state)) 
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -36,6 +36,10 @@ export function getCurrentUser(){
     return null
   }
 }
+export function signOut(){
+  AV.User.logOut()
+  return undefined
+}
 
 function getUserFromAVUser(AVUser){
   return {
