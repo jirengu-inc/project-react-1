a566d293c6d564131f180b97b93e8908b33dbf99
frankfang <frankfang1990@gmail.com>
2017-04-18 15:52:05 +0800

    注册成功后，记住 user，显示 username

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
+      user: {},
       newTodo: '',
       todoList: []
     }
@@ -29,7 +30,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>我的待办</h1>
+        <h1>{this.state.user.username||'我'}的待办</h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo} 
             onChange={this.changeTitle.bind(this)}
@@ -38,9 +39,13 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        <UserDialog />
+        <UserDialog onSignUp={this.onSignUp.bind(this)}/>
       </div>
     )
+  }
+  onSignUp(user){
+    this.state.user = user
+    this.setState(this.state)
   }
   componentDidUpdate(){
   }
--- src/UserDialog.js
+++ src/UserDialog.js
@@ -21,7 +21,7 @@ export default class UserDialog extends Component{
     e.preventDefault()
     let {username, password} = this.state.formData
     let success = (user)=>{
-      console.log(user)
+      this.props.onSignUp.call(null, user)
     }
     let error = (error)=>{
       console.log(error)
