1488720c2ef57bff82545b8b556b4154988828dc
frankfang <frankfang1990@gmail.com>
2017-05-11 20:13:03 +0800

    将 SignUpForm 抽离成一个组件（重要）

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './UserDialog.css'
 import {signUp, signIn, sendPasswordResetEmail} from './leanCloud'
+import SignUpForm from './SignUpForm'
 export default class UserDialog extends Component{
   constructor(props){
     super(props)
@@ -61,29 +62,6 @@ export default class UserDialog extends Component{
     this.setState(stateCopy)
   }
   render(){
-    let signUpForm = (
-      <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
-        <div className="row">
-          <label>邮箱</label> 
-          <input type="text" value={this.state.formData.email}
-            onChange={this.changeFormData.bind(this, 'email')}/>
-        </div>
-        <div className="row">
-          <label>用户名</label> 
-          <input type="text" value={this.state.formData.username}
-            onChange={this.changeFormData.bind(this, 'username')}/>
-          {/* bind 不仅可以绑定 this，还可以绑定第一个参数 */}
-        </div>
-        <div className="row">
-          <label>密码</label>
-          <input type="password" value={this.state.formData.password} 
-            onChange={this.changeFormData.bind(this, 'password')}/>
-        </div>
-        <div className="row actions">
-          <button type="submit">注册</button>
-        </div>
-      </form>
-    )
     let signInForm = (
       <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
         <div className="row">
@@ -117,7 +95,11 @@ export default class UserDialog extends Component{
             /> 登录</label>
         </nav>
         <div className="panes">
-          {this.state.selected === 'signUp' ? signUpForm : null}
+          {this.state.selected === 'signUp' ?
+            <SignUpForm formData={this.state.formData}
+              onSubmit={this.signUp.bind(this)}
+              onChange={this.changeFormData.bind(this)}/>
+            : null}
           {this.state.selected === 'signIn' ? signInForm : null}
         </div>
       </div>
