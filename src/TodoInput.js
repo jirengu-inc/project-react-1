ad5affdf389fc438c157ada536a6e565f2474c46
frankfang <frankfang1990@gmail.com>
2017-05-22 23:41:23 +0800

    如果用户在输入框里什么都不写就敲回车，那么就拒绝用户

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './TodoInput.css'
 
 function submit (props, e) {
   if (e.key === 'Enter') {
-    props.onSubmit(e)
+    if (e.target.value.trim() !== '') {
+      props.onSubmit(e)
+    }
   }
 }
 function changeTitle (props, e) {
