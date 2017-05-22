870cffc590d2829fd2b88ac57dcc68fc81faf0a1
frankfang <frankfang1990@gmail.com>
2017-05-23 01:29:09 +0800

    删除 todo

--- src/App.js
+++ src/App.js
@@ -99,8 +99,10 @@ class App extends Component {
     })
   }
   delete(event, todo){
-    todo.deleted = true
-    this.setState(this.state)
+    TodoModel.destroy(todo.id, () => {
+      todo.deleted = true
+      this.setState(this.state)
+    })
   }
 }
 
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -48,8 +48,14 @@ export const TodoModel = {
   update(){
 
   },
-  destroy(){
-
+  destroy(todoId, successFn, errorFn){
+    // 文档 https://leancloud.cn/docs/leanstorage_guide-js.html#删除对象
+    let todo = AV.Object.createWithoutData('Todo', todoId)
+    todo.destroy().then(function (response) {
+      successFn && successFn.call(null)
+    }, function (error) {
+      errorFn && errorFn.call(null, error)
+    });
   }
 }
 
